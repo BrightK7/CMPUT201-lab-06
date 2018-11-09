e129923554d949e077fef7a9a937cc7d1aca044f
Bo Li <bo5@cs.ualberta.ca>
2018-11-08 21:48:04 -0700

    first

---
+++ 374.c
@@ -0,0 +1,11 @@
+int main(int argc, char const *argv[])
+{
+    int a[=10],i,j,k,m;
+    i = j;
+    i = 10 * j +1;
+    i = j - k;
+    i = (((j)*(j)) * ((j)*(j)));
+    i = ((j)*(j)*(j));
+
+    return 0;
+}
