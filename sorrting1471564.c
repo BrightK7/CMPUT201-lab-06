c0e9428f3ae816d29e530e59434045ebfae70c4f
Bo Li <bo5@cs.ualberta.ca>
2018-11-29 15:03:17 -0700

    sorting

---
+++ sorrting1471564.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "quicksort.h"
+
+
+int main(int argc, char const *argv[])
+{
+	if (argc != 2){										// if input != 2 printf the error in stderr
+		fprintf(stderr,"./sorting -q|-m|-i|b|s|h\n");
+		return 0;
+	}
+    int num;
+    printf("Enther the interger to be sort: ");
+    scanf("%d",&num);
+    int *sort_num = malloc(num*sizeof(int));
+    for (int i = 0; i < num; i++){
+        scanf("%d",sort_num + i);
+    }
+    printf("The input order for the  %d intergers is ",num);
+    for (int i = 0; i < num; i++){
+        printf(" %d",sort_num[i]);
+
+    }
+    printf("\n");
+    
+    if(strcmp(argv[1],"-q") ==0){
+        quicksort(sort_num,0,num-1);
+
+
+
+    }	
+    free(sort_num);
+    return 0;
+}
